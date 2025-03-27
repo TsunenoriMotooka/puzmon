014c2bdea3b014f9d499c428598d9a75887bc4e9
TsunenoriMotooka <tsune.motooka@gmail.com>
2025-03-27 14:27:19 +0900

    パーティの実装

--- puzmon.py
+++ puzmon.py
@@ -4,6 +4,7 @@ author: tsune.motooka
 '''
 
 # import
+import math
 
 # const
 ELEMENT_SYMBOLS = {
@@ -23,7 +24,6 @@ ELEMENT_COLORS = {
         '無': '7',
         }
     
-
 # class
 class Monster:
     def __init__(self, name, max_hp, element, ap, dp):
@@ -41,12 +41,19 @@ class Monster:
 
 # data
 monsters = [
-    Monster('スライム',    100, '水', 10,  1),
-    Monster('ゴブリン',    200, '土', 20,  5),
-    Monster('オオコウモリ',300, '風', 30, 10),
-    Monster('ウェアウルフ',400, '風', 40, 15),
-    Monster('ドラゴン',    600, '火', 50, 20),
-    ]
+        Monster('スライム',    100, '水', 10,  1),
+        Monster('ゴブリン',    200, '土', 20,  5),
+        Monster('オオコウモリ',300, '風', 30, 10),
+        Monster('ウェアウルフ',400, '風', 40, 15),
+        Monster('ドラゴン',    600, '火', 50, 20),
+        ]
+
+friends = [
+        Monster('青龍', 150, '風', 15, 10),
+        Monster('朱雀', 150, '火', 25, 10),
+        Monster('白虎', 150, '土', 20,  5),
+        Monster('玄武', 150, '水', 20, 15),
+        ]        
 
 # function
 def main():
@@ -54,7 +61,9 @@ def main():
 
     print('*** Puzzle & Monsters ***')
 
-    win_count = go_dungeon(player_name)
+    party = organize_party(player_name, friends)
+
+    win_count = go_dungeon(party, monsters)
 
     if win_count >= len(monsters):
         print('*** GAME CLEARED!! ***')
@@ -71,19 +80,22 @@ def input_player_name():
     
     return player_name
 
-def go_dungeon(player_name):
+def go_dungeon(party, monsters):
     win_count = 0
-    print(f'{player_name}はダンジョンに到着した')
-   
+    print(f'{party['player_name']}のパーティ(HP={party['hp']})はダンジョンに到着した')
+    show_party(party)
     #モンスターとの戦闘
     for monster in monsters:
-        win = do_battle(monster)
-        if win > 0:
-            win_count += win
-        else:
-            break
+        win_count += do_battle(monster)
 
-    print(f'{player_name}はダンジョンを制覇した')
+        if party['hp'] > 0:
+            print(f'{party['player_name']}はさらに奥へと進んだ')
+            print(f'{'='*32}')
+        else:
+            print(f'{party['player_name']}はダンジョンから逃げ出した')
+            break
+    else:
+        print(f'{party['player_name']}はダンジョンを制覇した')
 
     return win_count
 
@@ -94,6 +106,27 @@ def do_battle(monster):
     print(f'を倒した！')
     return 1
 
+def organize_party(player_name, friends):
+    sum_hp = sum([friend.hp for friend in friends])
+    sum_max_hp = sum([friend.max_hp for friend in friends])
+    avg_dp = math.ceil(sum([friend.dp for friend in friends]) / len(friends))
+
+    party = {
+            'player_name': player_name,
+            'friends': friends,
+            'hp': sum_hp,
+            'max_hp': sum_max_hp,
+            'dp': avg_dp
+            }
+    
+    return party
+
+def show_party(party):
+    print(f'＜パーティ編成＞----------------')
+    for friend in party['friends']:
+        friend.print_name()
+        print(f' HP= {friend.hp:3} 攻撃= {friend.ap:2} 防御= {friend.dp:2}')
+    print(f'{'-'*32}\n')
 
 # start app
 main()
