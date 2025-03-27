4f904b46e5dd8eaae3bf41666324060e7bb56f50
TsunenoriMotooka <tsune.motooka@gmail.com>
2025-03-27 13:24:17 +0900

    モンスターの実装

--- puzmon.py
+++ puzmon.py
@@ -6,13 +6,47 @@ author: tsune.motooka
 # import
 
 # const
-monster_names = [
-        'スライム',
-        'ゴブリン',
-        'オオコウモリ',
-        'ウェアウルフ',
-        'ドラゴン',
-        ]
+ELEMENT_SYMBOLS = {
+        '火': '$',
+        '水': '~',
+        '風': '@',
+        '土': '#',
+        '命': '&',
+        '無': ' ',
+        }
+ELEMENT_COLORS = {
+        '火': '1',
+        '水': '6',
+        '風': '2',
+        '土': '3',
+        '命': '5',
+        '無': '7',
+        }
+    
+
+# class
+class Monster:
+    def __init__(self, name, max_hp, element, ap, dp):
+        self.name = name
+        self.hp = max_hp
+        self.max_hp = max_hp
+        self.element = element
+        self.ap = ap
+        self.dp = dp
+
+    def print_name(self):
+        symbol = ELEMENT_SYMBOLS[self.element]
+        color  = '3' + ELEMENT_COLORS[self.element]
+        print(f'\033[{color}m{symbol}{self.name}{symbol}\033[0m', end='')
+
+# data
+monsters = [
+    Monster('スライム',    100, '水', 10,  1),
+    Monster('ゴブリン',    200, '土', 20,  5),
+    Monster('オオコウモリ',300, '風', 30, 10),
+    Monster('ウェアウルフ',400, '風', 40, 15),
+    Monster('ドラゴン',    600, '火', 50, 20),
+    ]
 
 # function
 def main():
@@ -22,7 +56,7 @@ def main():
 
     win_count = go_dungeon(player_name)
 
-    if win_count >= len(monster_names):
+    if win_count >= len(monsters):
         print('*** GAME CLEARED!! ***')
     else:
         print('*** GAME OVER!! ***')
@@ -42,8 +76,8 @@ def go_dungeon(player_name):
     print(f'{player_name}はダンジョンに到着した')
    
     #モンスターとの戦闘
-    for monster_name in monster_names:
-        win = do_battle(monster_name)
+    for monster in monsters:
+        win = do_battle(monster)
         if win > 0:
             win_count += win
         else:
@@ -53,9 +87,11 @@ def go_dungeon(player_name):
 
     return win_count
 
-def do_battle(monster_name):
-    print(f'{monster_name}が現れた！')
-    print(f'{monster_name}を倒した！')
+def do_battle(monster):
+    monster.print_name()
+    print(f'が現れた！')
+    monster.print_name()
+    print(f'を倒した！')
     return 1
 
 
