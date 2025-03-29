cbeed70433912f6ac1bc6e807ac2060a92f79fac
TsunenoriMotooka <tsune.motooka@gmail.com>
2025-03-29 14:03:57 +0900

    ファイルを分ける

--- puzmon.py
+++ puzmon.py
@@ -4,161 +4,13 @@ author: tsune.motooka
 '''
 
 # import
-import math
 import random
 
-# const
-ELEMENT_NONE = '無'
-ELEMENT_LIFE = '命'
+from const import *
+from monster import Monster
+from party import Party
 
-ELEMENT_SYMBOLS = {
-        '火': '$',
-        '水': '~',
-        '風': '@',
-        '土': '#',
-        '命': '&',
-        '無': ' ',
-        }
-ELEMENT_COLORS = {
-        '火': '1',
-        '水': '6',
-        '風': '2',
-        '土': '3',
-        '命': '5',
-        '無': '7',
-        }
-
-ELEMENT_BOOST = {
-        '火風': 2.0,
-        '火水': 0.5,
-        '水火': 2.0,
-        '水土': 0.5,
-        '風土': 2.0,
-        '風火': 0.5,
-        '土水': 2.0,
-        '土風': 0.5,
-        }
-
-GEMS_LENGTH = 14
-
-LINE_LENGTH = 34
-
-# class
-class Monster:
-    def __init__(self, name, max_hp, element, ap, dp):
-        self.name = name
-        self.hp = max_hp
-        self.max_hp = max_hp
-        self.element = element
-        self.ap = ap
-        self.dp = dp
-
-    def print_name(self):
-        symbol = ELEMENT_SYMBOLS[self.element]
-        color  = '4' + ELEMENT_COLORS[self.element]
-        print(f'\033[{color}m\033[30m{symbol}{self.name}{symbol}\033[0m', end='')
-
-class Party:
-    def __init__(self, name, friends):
-        self.name = name
-        self.friends = friends
-        self.hp = sum([friend.hp for friend in friends])
-        self.max_hp = sum([friend.max_hp for friend in friends])
-        self.dp = math.ceil(sum([friend.dp for friend in friends]) / len(friends))
-        self.gems = self.fill_gems(GEMS_LENGTH)
-
-    def show(self):
-        print(f'＜パーティ編成＞')
-        print(f'{'-'*LINE_LENGTH}')
-        for friend in self.friends:
-            friend.print_name()
-            print(f' HP= {friend.hp:3} 攻撃= {friend.ap:2} 防御= {friend.dp:2}')
-        print(f'{'-'*LINE_LENGTH}\n')
-
-    def fill_gems(self, count):
-        gems = [random.choice(list(ELEMENT_SYMBOLS.keys())[0:-1]) for i in range(count)]
-        return gems
-
-    def show_gems(self, banish_element=None, banish_count=0):
-        for i, element in enumerate(self.gems, 0):
-            print(f'{' ' if i > 0 else ''}', end='')
-            self.show_gem(element)
-
-        if banish_element is not None:
-            print('  ', end='')
-            self.show_gem(banish_element)
-            print(f'x{banish_count}', end='')
-
-        print()
-
-    def show_gem(self, element):
-        color  = '4' + ELEMENT_COLORS[element]
-        print(f'\033[{color}m\033[30m{ELEMENT_SYMBOLS[element]}\033[0m', end='')
-
-    def move_gem(self, beforeIndex, afterIndex, output=True):
-        if beforeIndex < 0 or beforeIndex >= len(self.gems):
-            return
-        if afterIndex < 0 or afterIndex >= len(self.gems):
-            return
-
-        step = 1 if beforeIndex < afterIndex else -1 
-        [self.swap_gem(i, step, output) for i in range(beforeIndex, afterIndex, step)]
-
-    def swap_gem(self, index, step, output=True): 
-        if index < 0 or index + step < 0 or index >= len(self.gems) or index + step >= len(self.gems):
-            return
-
-        temp = self.gems[index + step]
-        self.gems[index + step] = self.gems[index]
-        self.gems[index] = temp
-
-        if output:
-            self.show_gems()
-    
-    def check_banishable(self):
-        for i in range(GEMS_LENGTH - 2):
-            banish_list = {i}
-            element = self.gems[i]
-            if element == ELEMENT_NONE:
-                break
-
-            for j in range(i + 1, GEMS_LENGTH):
-                if element == self.gems[j]:
-                    banish_list.add(j)
-                else:
-                    break
-            if len(banish_list) >=3:
-                return (element, banish_list)
-        return (None, None)
-
-    def banish_gems(self):
-        banish_element, banish_list = self.check_banishable()
-        if len(banish_list) > 0:
-            for i in banish_list:
-                self.gems[i] = ELEMENT_NONE
-            self.show_gems(banish_element, len(banish_list))
-
-    def shift_gems(self):
-        for i in range(len(self.gems) - 1, -1, -1):
-            if self.gems[i] == ELEMENT_NONE:
-                self.move_gem(i, len(self.gems) - 1, False)
-        self.show_gems()
-
-    def spawn_gems(self):
-        on_spawn = False
-        for i in range(len(self.gems)):
-            if self.gems[i] == ELEMENT_NONE:
-                self.gems[i] = self.fill_gems(1)[0]
-                on_spawn = True
-        if on_spawn:
-            self.show_gems()
-
-    def get_friend_by_element(self, element):
-        friends = [friend for friend in self.friends if friend.element == element]
-        if len(friends) > 0:
-            return friends[0]
-
-# function
+# main
 def main():
     player_name = input_player_name()
 
@@ -179,7 +31,7 @@ def main():
             Monster('白虎', 150, '土', 20,  5),
             Monster('玄武', 150, '水', 20, 15),
             ]        
-    party = organize_party(player_name, friends)
+    party = Party(player_name, friends)
 
     win_count = go_dungeon(party, enemys)
     if win_count >= len(enemys):
@@ -216,31 +68,28 @@ def go_dungeon(party, enemys):
 
     return win_count
 
-def organize_party(player_name, friends):
-    return Party(player_name, friends)
-
 def on_player_turn(party, enemy):
     print(f'\n【{party.name}のターン】(HP={party.hp})')
     show_battle_field(party, enemy)
     command = input_command()
+
     before = command[0:1].upper()
     after = command[-1:].upper()
     beforeIndex = ord(before) - 65 
     afterIndex = ord(after) - 65
-    party.move_gem(beforeIndex, afterIndex)
-   
+    party.gems.move(beforeIndex, afterIndex)
     evaluate_gems(party, enemy)
             
 def evaluate_gems(party, enemy, combo=0, on_spawn=False):
     while True: 
         if on_spawn:
-            party.spawn_gems()
+            party.gems.spawn()
 
-        element, banish_list = party.check_banishable()
+        element, banish_list = party.gems.check_banishable()
         if element is None:
             break
 
-        party.banish_gems()
+        party.gems.banish()
     
         if element == ELEMENT_LIFE:
             do_recover(party, len(banish_list), combo)
@@ -252,7 +101,7 @@ def evaluate_gems(party, enemy, combo=0, on_spawn=False):
                 combo += 1
                 do_attack(friend, enemy, len(banish_list), combo)
 
-        party.shift_gems()
+        party.gems.shift()
 
     if on_spawn != True:
         evaluate_gems(party, enemy, combo, on_spawn=True) 
@@ -322,7 +171,7 @@ def show_battle_field(party, enemy):
     print(f'{'-'*LINE_LENGTH}')
     [print(f'{' ' if i > 0 else ''}{chr(i+65)}', end='') for i in range(GEMS_LENGTH)]
     print()
-    party.show_gems()
+    party.gems.show()
     print(f'{'-'*LINE_LENGTH}')
 
 def input_command():
